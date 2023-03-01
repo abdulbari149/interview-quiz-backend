c672ae6571d1a9fcc41b8b49c965830d776e9b66
abdulbari149 <abdulbari131103@gmail.com>
2023-03-01 12:17:32 +0500

    error changes and get user by id

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,15 +1,43 @@
+const { Sequelize, ValidationError } = require("sequelize");
 const { models } = require("../models");
 
 const createUser = async (req, res) => {
 	try {
 		const { name, cnic } = req.body;
+		const userExists = await models.User.findOne({
+			where: {
+				cnic
+			}
+		})
+		if (userExists) {
+			const error = new Error("CNIC already exists")
+			error.status = 402
+			throw error;
+		}
 		const user = await models.User.create({ name, cnic });
 		res.status(201).send(user);
 	} catch (error) {
-		res.status(500).send({ message: error.message });
+		res.status(error?.status ?? 500).json({ message: error.message })
 	}
 };
 
+const getUserById = async (req, res, next) => {
+	try {
+		const userId = parseInt(req.params.id, 10);
+		console.log(typeof userId)
+		if (isNaN(userId) || userId <= 0) {
+			return res.status(400).json({ message: "User should be a number" });
+		}
+		const user = await models.User.findByPk(userId);
+		if (!user) {
+			return res.status(404).json({ message: "User not found" });
+		}
+		res.status(200).json(user);
+	} catch (error) {
+		res.status(500).json({ message: error.message });
+	}
+};
 module.exports = {
 	createUser,
+	getUserById
 };
--- src/routes/user.js
+++ src/routes/user.js
@@ -5,5 +5,6 @@ const userController = require("../controllers/user");
 
 const router = express.Router();
 router.post("/", userController.createUser);
+router.get("/:id", userController.getUserById);
 
 module.exports = router;
